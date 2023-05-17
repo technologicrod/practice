e81686b2d7bde197b6ea2ebf437367012fd4c9d1
technologicrod <matchon.rodwell@gmail.com>
2023-05-17 15:16:25 +0800

    Second commit

--- server/index.js
+++ server/index.js
@@ -6,13 +6,21 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const port = process.env.PORT || 8000;
+app.listen(port)
 
 app.get("/", (req,res) => {
+    res.sendFile(
+        path.join(_dirname, "../client/src/App.js"),
+        function(err){
+            if(err){
+                res.status(500).send(err)
+            }
+        }
+    )
+})
+app.get("/hello", (req,res) => {
     let name = "Rodwell"
     console.log("hello world", name)
     res.json(name)
-})
-
-app.listen(8000, () => {
-    console.log("running on port 8000");
 })
